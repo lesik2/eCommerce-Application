cba0e18f456e86e507b3b20178f43e32a9436feb
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2023-08-06 12:02:16 +0300

    feat: configure eslint and prettier

--- ecommerce-application/.eslintrc.cjs
+++ ecommerce-application/.eslintrc.cjs
@@ -2,17 +2,34 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin: prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react-refresh',
+    'prettier',
+    '@typescript-eslint',
+],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope':0,
+    'no-debugger': 'off',
+    'no-console': 0,
+    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/no-var-requires': 0,
   },
 }
