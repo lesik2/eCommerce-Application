468b2294c42417a14d171d6577a1bf07ed223360
“Irina <irina.osipova2012@gmail.com>
2023-08-09 21:50:56 +0200

    chore: install node polyfills

--- ecommerce-application/vite.config.ts
+++ ecommerce-application/vite.config.ts
@@ -1,9 +1,13 @@
+/* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +16,30 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         setupFiles: ['./src/setupTests.ts'],
+    },
+    envDir: './', // The path to the directory containing your .env file (default: './')
+    resolve: {
+        alias: {
+            stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+        },
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            define: {
+                global: 'globalThis',
+            },
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    process: true,
+                    buffer: true,
+                }),
+                NodeModulesPolyfillPlugin(),
+            ],
+        },
+    },
+    build: {
+        rollupOptions: {
+            plugins: [rollupNodePolyFill()],
+        },
     },
 });
