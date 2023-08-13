e961161109ac97014a01e356081be1672600b1f8
baranovdv <baranov.d.v.1988@gmail.com>
2023-08-13 16:23:49 +0700

    feat: add routing

--- ecommerce-application/src/main.tsx
+++ ecommerce-application/src/main.tsx
@@ -1,10 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './Root';
 import './index.css';
+import Login from './pages/Login';
+import Registration from './pages/Registrarion';
+import Menu from './pages/Menu';
+import NavBar from './components/NavBar';
+import Soup from './pages/Soup';
+import Poke from './pages/Poke';
+import Beverages from './pages/Beverages';
+import Cart from './pages/Cart';
+import About from './pages/About';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        children: [
+            {
+                path: '/',
+                element: <NavBar />,
+                children: [
+                    {
+                        path: 'menu',
+                        element: <Menu />,
+                    },
+                    {
+                        path: 'soup',
+                        element: <Soup />,
+                    },
+                    {
+                        path: 'poke',
+                        element: <Poke />,
+                    },
+                    {
+                        path: 'beverages',
+                        element: <Beverages />,
+                    },
+                    {
+                        path: 'cart',
+                        element: <Cart />,
+                    },
+                    {
+                        path: 'about',
+                        element: <About />,
+                    },
+                ],
+            },
+            {
+                path: 'registration',
+                element: <Registration />,
+            },
+            {
+                path: 'login',
+                element: <Login />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
