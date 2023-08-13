a5a0bed09a99ea061ae646376eb9d03349311159
baranovdv <baranov.d.v.1988@gmail.com>
2023-08-13 15:51:29 +0700

    feat: add Header

---
+++ ecommerce-application/src/data/header-data.ts
@@ -0,0 +1,8 @@
+const HeaderData = {
+    titleLine1: 'Delivery of Asian',
+    titleLine2: 'Cuisine',
+    subtitleLine1: '06 593 97 301',
+    subtitleLine2: 'we deliver 10 a.m. to 00:00 a.m.',
+};
+
+export default HeaderData;
