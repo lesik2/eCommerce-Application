6f0df26d065fbf38c752bd51d117077f38ba95c8
baranovdv <baranov.d.v.1988@gmail.com>
2023-08-13 16:15:50 +0700

    feat: add Navbar

---
+++ ecommerce-application/src/data/navbar-data.ts
@@ -0,0 +1,29 @@
+const NavLinks = [
+    {
+        id: 'link1',
+        name: 'MENU',
+        url: '/menu',
+    },
+    {
+        id: 'link2',
+        name: 'POKE BOWL',
+        url: '/poke',
+    },
+    {
+        id: 'link3',
+        name: 'SOUP',
+        url: '/soup',
+    },
+    {
+        id: 'link4',
+        name: 'BEVERAGES',
+        url: '/beverages',
+    },
+    {
+        id: 'link5',
+        name: 'ABOUT US',
+        url: '/about',
+    },
+];
+
+export default NavLinks;
