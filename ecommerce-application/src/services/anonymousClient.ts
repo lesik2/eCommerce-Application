9ea82cd690d1847734fdc9f622618169eef76d42
“Irina <irina.osipova2012@gmail.com>
2023-08-13 16:36:03 +0200

    feat: restructure anon client

--- ecommerce-application/src/services/anonymousClient.ts
+++ ecommerce-application/src/services/anonymousClient.ts
@@ -1,15 +1,49 @@
-import { ClientBuilder } from '@commercetools/sdk-client-v2';
+import {
+    ClientBuilder,
+    AnonymousAuthMiddlewareOptions,
+} from '@commercetools/sdk-client-v2';
 import {
     createApiBuilderFromCtpClient,
     ApiRoot,
 } from '@commercetools/platform-sdk';
-import {
-    authMiddlewareOptions,
-    httpMiddlewareOptions,
-} from './middlewareOptions';
+import { httpMiddlewareOptions } from './credentialsFlow';
+import SCOPES from './scopes';
 
+export const anonAuthMiddlewareOptions: AnonymousAuthMiddlewareOptions = {
+    host: import.meta.env.VITE_AUTH_URL,
+    projectKey: import.meta.env.VITE_PROJECT_KEY,
+    credentials: {
+        clientId: import.meta.env.VITE_CLIENT_ID,
+        clientSecret: import.meta.env.VITE_CLIENT_SECRET,
+    },
+    scopes: SCOPES,
+    fetch,
+    tokenCache: {
+        get: () => {
+            const cacheKey = `token`;
+            const cachedToken = localStorage.getItem(cacheKey);
+            if (cachedToken) {
+                console.log(cachedToken);
+                return {
+                    token: `Bearer: ${JSON.parse(cachedToken)}`,
+                    expirationTime: 0,
+                    refreshToken: '',
+                };
+            }
+            return { token: '', expirationTime: 0, refreshToken: '' };
+        },
+        set: (cache) => {
+            const cacheKey = `token`;
+            const cachedToken = localStorage.getItem(cacheKey);
+            if (!cachedToken)
+                localStorage.setItem(cacheKey, JSON.stringify(cache.token));
+        },
+    },
+};
+
+// create anonymousClient when unauthorized user add smth in the cart
 export const anonymousClient = new ClientBuilder()
-    .withAnonymousSessionFlow(authMiddlewareOptions)
+    .withAnonymousSessionFlow(anonAuthMiddlewareOptions)
     .withHttpMiddleware(httpMiddlewareOptions)
     .withLoggerMiddleware()
     .build();
@@ -17,12 +51,13 @@ export const anonymousClient = new ClientBuilder()
 export const getApiRootAnon: () => ApiRoot = () => {
     return createApiBuilderFromCtpClient(anonymousClient);
 };
-// export const products = await getApiRootAnon()
-//     .withProjectKey({
-//         projectKey: import.meta.env.VITE_PROJECT_KEY,
-//     })
-//     .productProjections()
-//     .get()
-//     .execute()
-//     .then((res) => res.body.results.map((el) => el.name))
-//     .catch((error) => console.error('Product request error ', error));
+
+export const products = await getApiRootAnon()
+    .withProjectKey({
+        projectKey: import.meta.env.VITE_PROJECT_KEY,
+    })
+    .productProjections()
+    .get()
+    .execute()
+    .then((res) => res.body.results.map((el) => el.name))
+    .catch((error) => console.error('Product request error ', error));
