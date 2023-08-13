3846430ef14954c0168a53f2493cf3dc30fcf5f0
baranovdv <baranov.d.v.1988@gmail.com>
2023-08-13 16:18:15 +0700

    feat: add pages base layout

---
+++ ecommerce-application/src/pages/Registrarion.tsx
@@ -0,0 +1,5 @@
+function Registration() {
+    return <h1 className="py-2 text-2xl text-center">Registration page</h1>;
+}
+
+export default Registration;
---
+++ ecommerce-application/src/pages/Soup.tsx
@@ -0,0 +1,5 @@
+function Soup() {
+    return <h1 className="py-2 text-2xl text-center">Soup page</h1>;
+}
+
+export default Soup;
