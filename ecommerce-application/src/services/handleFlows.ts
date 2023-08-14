4fb74035328e9224a68d3bc33642fe8f7bfed36d
“Irina <irina.osipova2012@gmail.com>
2023-08-14 18:30:45 +0200

    feat: add flows handle

---
+++ ecommerce-application/src/services/handleFlows.ts
@@ -0,0 +1,31 @@
+import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
+import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+import { ClientBuilder } from '@commercetools/sdk-client-v2';
+import { getApiRoot, httpMiddlewareOptions } from './credentialsFlow';
+
+export default function handleFlows(): ByProjectKeyRequestBuilder {
+    const tokenData = localStorage.getItem('token');
+    const state = localStorage.getItem('status');
+    console.log('handleFlows');
+    console.log(tokenData, state);
+
+    if (state && tokenData) {
+        console.log('with token');
+        const client = new ClientBuilder()
+            .withExistingTokenFlow(`Bearer ${JSON.parse(tokenData).token}`, {
+                force: true,
+            })
+            .withHttpMiddleware(httpMiddlewareOptions)
+            .withLoggerMiddleware()
+            .build();
+        const requestBuilder = createApiBuilderFromCtpClient(
+            client
+        ).withProjectKey({
+            projectKey: import.meta.env.VITE_PROJECT_KEY,
+        });
+        return requestBuilder;
+    }
+    return getApiRoot().withProjectKey({
+        projectKey: import.meta.env.VITE_PROJECT_KEY,
+    });
+}
