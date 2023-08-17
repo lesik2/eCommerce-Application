cfff3095266044230f14c927b8284bca9cbbd1cb
“Irina <irina.osipova2012@gmail.com>
2023-08-17 22:46:13 +0200

    feat: add registration logic to login

--- ecommerce-application/src/pages/Login/Login.tsx
+++ ecommerce-application/src/pages/Login/Login.tsx
@@ -1,42 +1,67 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import Button from '@mui/material/Button';
+import { useState, useEffect, useMemo } from 'react';
 import { AlertColor } from '@mui/material/Alert';
 import handleLogin from '../../services/login';
 import FetchResultAlert from '../../components/FetchResultAlert';
 import { loginErrorMappings } from '../../services/errors/errors';
+import CustomizedButton from '../../components/ui/CustomizedButton';
+import { Inputs } from '../../data/data';
+import FormInput from '../Registration/components/formInput';
 
 function Login() {
     const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [values, setValues] = useState({
+        firstname: '',
+        lastname: '',
+        birthday: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        country: '',
+        street: '',
+        city: '',
+        postalCode: '',
+    });
+    const [validInputs, setValidInputs] = useState({
+        firstname: false,
+        lastname: false,
+        birthday: false,
+        email: false,
+        password: false,
+        confirmPassword: false,
+        street: false,
+        city: false,
+        country: false,
+        postalCode: false,
+    });
     const [successMessage, setSuccessMessage] = useState('');
     const [sev, setSeverity] = useState<AlertColor>('error');
     const [error, setError] = useState('');
 
+    const disableButton = useMemo(() => {
+        const validValues = [validInputs.email, validInputs.password];
+        const Values = [values.email, values.password];
+        const validation = validValues.includes(false);
+        const emptyValues = Values.includes('');
+        return validation || emptyValues;
+    }, [validInputs, values]);
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
     useEffect(() => {
-        if (
-            localStorage.getItem('token') &&
-            localStorage.getItem('status') === 'loggedIn'
-        ) {
+        if (localStorage.getItem('token') && localStorage.getItem('status') === 'loggedIn') {
             navigate('../');
         }
     }, [navigate]);
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
-        const loginData = await handleLogin(email, password)
-            .then((data) => {
-                setSuccessMessage(
-                    `Successful login. You'll be redirected to the main page`
-                );
+        const loginData = await handleLogin(values.email, values.password)
+            .then(() => {
+                setSuccessMessage(`Successful login. You'll be redirected to the main page`);
                 setSeverity('success');
                 setError('');
-                if (data) {
-                    // to be used for greeting on main page
-                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                    const clientName = data.clientData.customer.firstName;
-                }
                 setTimeout(() => {
                     navigate('../');
                 }, 500);
@@ -45,66 +70,53 @@ function Login() {
                 if (e.message === 'invalid_token') {
                     localStorage.removeItem('token');
                 }
-                const message =
-                    loginErrorMappings[e.body.errors[0].code] || `${e.message}`;
+                const message = loginErrorMappings[e.body.errors[0].code] || `${e.message}`;
                 setError(message);
                 setSeverity('error');
             });
         return loginData;
     };
     return (
-        <>
-            <h1 className="py-2 text-2xl text-center">Login</h1>
-            <p>Do not have an account?</p>
-            <Link
-                className="block text-center py-2 text-xl text-[blue] underline"
-                to="../registration"
-            >
-                to registration
-            </Link>
-            <form className="register-form" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">
-                        Email:
-                        <input
-                            required
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            onFocus={() => {
-                                setError('');
-                                setSuccessMessage('');
-                            }}
-                        />
-                    </label>
+        <div className="register-wrapper">
+            <div className="register">
+                <div className="register-menu">
+                    <h2 className="register-title">LOGIN</h2>
+                    <div className="register-signIn">
+                        <p>Do not have an account?</p>
+                        <Link to="/registration">
+                            <CustomizedButton sx={{ fontSize: 15 }} variant="contained">
+                                +SIGN UP
+                            </CustomizedButton>
+                        </Link>
+                    </div>
                 </div>
-                <div>
-                    <label htmlFor="password">
-                        Password:
-                        <input
-                            required
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            onFocus={() => {
-                                setError('');
-                                setSuccessMessage('');
-                            }}
-                        />
-                    </label>
-                </div>
-                <Button
-                    sx={{ fontSize: 17, marginTop: 3 }}
-                    className="register-form__submit register-button"
-                    type="submit"
-                >
-                    SIGN UP
-                </Button>
-            </form>
-            {error && <FetchResultAlert severity={sev} message={error} />}
-            {successMessage && (
-                <FetchResultAlert severity={sev} message={successMessage} />
-            )}
-        </>
+                <form className="register-form" onSubmit={handleSubmit}>
+                    <fieldset className="register-form__user">
+                        {Inputs.filter((input) => input.name === 'email' || input.name === 'password').map((input) => (
+                            <FormInput
+                                key={input.id}
+                                input={input}
+                                value={input.name === 'email' ? values.email : values.password}
+                                onChange={onChange}
+                                validInputs={validInputs}
+                                setValidInputs={setValidInputs}
+                            />
+                        ))}
+                    </fieldset>
+                    {error && <FetchResultAlert severity={sev} message={error} />}
+                    {successMessage && <FetchResultAlert severity={sev} message={successMessage} />}
+                    <CustomizedButton
+                        type="submit"
+                        sx={{ fontSize: 17, marginTop: 5 }}
+                        className="register-form__submit"
+                        variant="contained"
+                        disabled={disableButton}
+                    >
+                        SIGN IN
+                    </CustomizedButton>
+                </form>
+            </div>
+        </div>
     );
 }
 
