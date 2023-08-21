8132b6d4a91c61d763573dd8ff40746fe07cc49c
baranovdv <baranov.d.v.1988@gmail.com>
2023-08-21 21:16:22 +0700

    feat: add links to login and registration to main page

--- ecommerce-application/src/pages/Main/Main.tsx
+++ ecommerce-application/src/pages/Main/Main.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+
 function Main() {
-    return <h1 className="py-2 text-2xl text-center">Welcome to Chilly!</h1>;
+    return (
+        <>
+            <h1 className="py-2 text-2xl text-center">Welcome to Chilly!</h1>
+            <Link className="block text-center py-2 text-xl text-[blue] underline" to="../login">
+                to login
+            </Link>
+            <Link className="block text-center py-2 text-xl text-[blue] underline" to="../registration">
+                to registration
+            </Link>
+        </>
+    );
 }
 
 export default Main;
