3afe08b2df78d280e5b1577aa5dc1977bb810c93
baranovdv <baranov.d.v.1988@gmail.com>
2023-08-22 15:53:30 +0700

    feat: add burger menu

--- ecommerce-application/src/components/ui/IconButton.tsx
+++ ecommerce-application/src/components/ui/IconButton.tsx
@@ -3,6 +3,7 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import LogoutIcon from '@mui/icons-material/Logout';
 import LoginIcon from '@mui/icons-material/Login';
 import PersonIcon from '@mui/icons-material/Person';
+import CloseIcon from '@mui/icons-material/Close';
 import { IconButtonTypes, ButtonSize } from '../../data/types';
 
 type ButtonProps = {
@@ -12,11 +13,12 @@ type ButtonProps = {
 
 export default function CreateIconButton({ type, size }: ButtonProps) {
     return (
-        <IconButton aria-label={type} size={size}>
+        <IconButton aria-label={type} size={size} color="inherit">
             {type === 'cart' && <ShoppingCartIcon fontSize="inherit" />}
             {type === 'login' && <LoginIcon fontSize="inherit" />}
             {type === 'logout' && <LogoutIcon fontSize="inherit" />}
             {type === 'logged' && <PersonIcon fontSize="inherit" />}
+            {type === 'close' && <CloseIcon fontSize="inherit" color="inherit" />}
         </IconButton>
     );
 }
--- ecommerce-application/src/data/types.ts
+++ ecommerce-application/src/data/types.ts
@@ -12,4 +12,4 @@ export type InputTypes =
 
 export type ButtonSize = 'small' | 'medium' | 'large';
 
-export type IconButtonTypes = 'cart' | 'login' | 'logout' | 'logged';
+export type IconButtonTypes = 'cart' | 'login' | 'logout' | 'logged' | 'close';
