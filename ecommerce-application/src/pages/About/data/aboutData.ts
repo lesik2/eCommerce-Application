78bd94d6493cb7f86a530483a4d2e6db54099469
“Irina <irina.osipova2012@gmail.com>
2023-09-18 19:13:38 +0200

    feat: add photo to about us page

--- ecommerce-application/src/pages/About/data/aboutData.ts
+++ ecommerce-application/src/pages/About/data/aboutData.ts
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import image from '../../../assets/img/face.png';
 import baranov_ava from '../../../assets/img/baranovdv.jpg';
+import osipova_ava from '../../../assets/img/osipovai.jpg';
 
 export interface IAboutUs {
     name: string;
@@ -28,7 +29,7 @@ const aboutIrina = {
     name: 'Irina',
     roles: ['CommerceTools setup', 'Product Cards', 'About us'],
     bio: 'After relocating to Poland in 2022, I started exploring a career transition. I began studying Frontend development at the end of 2022 with the Stage 0 RS School course. Prior to this, I worked for 11 years in the construction and design industries, initially as a project coordinator and later as a procurement manager.',
-    img: image,
+    img: osipova_ava,
     GitHub: 'https://github.com/IrinaOsp',
 };
 
