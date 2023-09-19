c04b679e541af9f59e32b25c7a0db31b47d2c1e0
baranovdv <baranov.d.v.1988@gmail.com>
2023-09-20 01:21:24 +0700

    refactor: refactor menu

--- ecommerce-application/src/components/ui/SubMenuItem.tsx
+++ ecommerce-application/src/components/ui/SubMenuItem.tsx
@@ -1,3 +1,5 @@
+// transition-all duration-${ANIM_TIME} ${filterMenu ? 'right-0' : 'right-[-320px]'}
+
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
@@ -30,15 +32,16 @@ export default function SubMenuItem(props: ItemsProps) {
                 onClick={(type === 'aside' && onClose) || undefined}
                 className={({ isActive }) =>
                     isActive
-                        ? `${type === 'main' && 'flex flex-col gap-1 hover:bg-bntActive/80  cursor-pointer'} ${
-                              type === 'aside' && 'hover:none'
-                          } px-[15px] py-[5px] rounded-3xl bg-bntActive`
-                        : `${type === 'main' && 'flex flex-col gap-1 hover:bg-btnHover  cursor-pointer'} ${
-                              type === 'aside' && 'hover:bg-bgBody'
-                          } px-[15px] py-[5px] rounded-3xl`
+                        ? `${
+                              type === 'main' &&
+                              'relative flex flex-col gap-1 hover:bg-bntActive/80  cursor-pointer z-40'
+                          } ${type === 'aside' && 'hover:none'} px-[15px] py-[5px] rounded-3xl bg-bntActive`
+                        : `${
+                              type === 'main' && 'relative flex flex-col gap-1 hover:bg-btnHover  cursor-pointer z-40'
+                          } ${type === 'aside' && 'hover:bg-bgBody'} px-[15px] py-[5px] rounded-3xl`
                 }
             >
-                <div className="flex items-center gap-1">
+                <div className="flex items-center gap-1 z-30">
                     {item.name}
                     {item.submenu && type === 'main' && <KeyboardArrowDownIcon fontSize="inherit" color="inherit" />}
                 </div>
@@ -49,12 +52,13 @@ export default function SubMenuItem(props: ItemsProps) {
 
             {item.submenu && (
                 <ul
-                    className={`absolute  w-auto px-[20px] py-[5px] rounded-3xl z-20 duration-${ANIM_TIME} ${
-                        type === 'main' && 'left-2/3 bg-btnHover'
+                    className={`absolute top-0  w-full px-[20px] py-[5px] rounded-3xl z-20 duration-${ANIM_TIME} ${
+                        type === 'main' && 'left-0 bg-btnHover'
                     } ${type === 'aside' && 'left-full bg-gray-400 transition-all'} ${
-                        dropdown ? 'top-8 visible opacity-100' : 'top-0 invisible opacity-0'
+                        dropdown ? 'visible opacity-100' : 'invisible opacity-0'
                     }`}
                 >
+                    {type === 'main' && <div className="h-[42px]" />}
                     {item.submenu.map((l) => (
                         <NavLink
                             key={l.id}
